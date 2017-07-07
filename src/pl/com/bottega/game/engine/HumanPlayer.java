f0d3ecae6e82a539152783e3c8524174cd8dda86
LukaszPawelec <nizari@o2.pl>
2017-07-08 00:36:56 +0200

    Initial commit (homework)

--- src/pl/com/bottega/game/engine/HumanPlayer.java
+++ src/pl/com/bottega/game/engine/HumanPlayer.java
@@ -2,29 +2,38 @@ package pl.com.bottega.game.engine;
 
 import java.util.Scanner;
 
-public class HumanPlayer implements Player{
+public class HumanPlayer implements Player {
 
     private String name;
-
     private Scanner scanner = new Scanner(System.in);
+    private int errorsCounter;
 
     public HumanPlayer(String name) {
         this.name = name;
     }
 
-    public String giveHand(){
+    public String giveHand() {
         System.out.println("Wpisz: kamien, nozyce, papier");
-
+        int counter = 0;
         String hand = scanner.nextLine();
-        if (!(hand.equals("kamien") || hand.equals("nozyce") || hand.equals("papier"))) {
-            System.out.println("input error");
-            return null;
+        while (!(hand.equals("kamien") || hand.equals("nozyce") || hand.equals("papier")) && counter < 3) {
+            System.out.println("try again");
+            hand = scanner.nextLine();
+            counter++;
+            counterAdder(counter);
         }
-
-        return hand;
+        if(hand.equals("kamien") || hand.equals("nozyce") || hand.equals("papier"))
+            return hand;
+        else
+            return null;
     }
 
-    public String getName(){
+    public String getName() {
         return name;
     }
+
+    private void counterAdder(int errors){
+        errorsCounter = (errorsCounter + errors);
+    }
+
 }
